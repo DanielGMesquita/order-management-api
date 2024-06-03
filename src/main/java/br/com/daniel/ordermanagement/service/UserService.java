52b6cbf35d463f7dab5ddf42a14c3162122719b6
DanielGMesquita <danielmesquitadev@gmail.com>
2024-06-02 23:55:14 -0300

    feat: activate security

--- src/main/java/br/com/daniel/ordermanagement/service/UserService.java
+++ src/main/java/br/com/daniel/ordermanagement/service/UserService.java
@@ -6,17 +6,47 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
-  @Autowired UserRepository userRepository;
+  @Autowired
+  UserRepository userRepository;
 
-  public User findByEmail(String email) {
-    return userRepository
-        .findByUserEmail(email)
-        .orElseThrow(() -> new EntityNotFoundException("User not found for email: " + email));
+  public List<User> getAll(){
+    return userRepository.findAll();
   }
 
-  public User save(User user) {
+  public User getById(Integer id) {
+    return userRepository.findById(id).orElse(null) ;
+  }
+
+  public User findByEmail(String email){
+    return userRepository.findByUserEmail(email).get();
+  }
+
+  public User saveUser(User user) {
     return userRepository.save(user);
+  }
+
+  public User updateUser(Integer id, User user) {
+    User updatedUser = userRepository.findById(id).orElse(null);
+    if(updatedUser != null) {
+      updatedUser.setUserEmail(user.getUserEmail());
+      updatedUser.setUserName(user.getUserName());
+      return userRepository.save(updatedUser);
+    }else {
+      return null;
+    }
+  }
+
+  public Boolean deleteUser(Integer id) {
+    User user = userRepository.findById(id).orElse(null);
+    if(user != null) {
+      userRepository.delete(user);
+      return true;
+    }else {
+      return false;
+    }
   }
 }
